48300a5f6b9c2b1dfd56140c0d54c106a0c0fece
Matthew Gall <me@matthewgall.dev>
2023-10-17 13:22:12 +0100

    Added improved validation of UUID handling for /uploads

--- functions/uploads/[uuid].js
+++ functions/uploads/[uuid].js
@@ -1,8 +1,20 @@
+import isUUID from 'validator/lib/isUUID';
+
 export async function handle({ request, env }) {
     let image = new URL(request.url).pathname.replace('/uploads/', '').toLowerCase().trim();
 
     let data;
     let headers;
+
+    if (!isUUID(image)) {
+        return new Response('Image not found. Please check and try again', {
+            status: 404,
+            headers: {
+                'Content-Type': 'text/plain'
+            }
+        })
+    }
+    
     try {
         let r2 = await env.R2.get(image);
         data = await r2.blob();
