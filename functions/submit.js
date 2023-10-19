29a3600d57ba8c3b213d402b68e717784d2daf1a
Matthew Gall <me@matthewgall.dev>
2023-10-20 00:34:59 +0100

    Moved submissionData to a new singular object to allow cleaned up code

--- functions/submit.js
+++ functions/submit.js
@@ -31,25 +31,34 @@ export async function handle({ request, env }) {
         return new Response(JSON.stringify(resp), {status: 400, headers: {'Content-Type': 'application/json'}})
     }
 
-    // Now we record the submission time
-    let submission_time = Math.floor(new Date().getTime() / 1000)
+    // Now we record the submission data
+    let submissionData = {
+        'firstName': formData.get('firstName', ''),
+        'lastName': formData.get('lastName', ''),
+        'othersName': formData.getAll('othersName').join(', ') || '',
+        'email': formData.get('email', null),
+        'phoneNumber': formData.get('phoneNumber', ''),
+        'locationDescription': formData.get('locationDescription', ''),
+        'need': formData.get('need', ''),
+        'otherNeed': formData.get('otherNeed', ''),
+        'usCitizen': formData.get('usCitizen', 'No'),
+        'latitude': formData.get('latitude') || null,
+        'longitude': formData.get('longitude') || null,
+        'files': [],
+        'createdAt': Math.floor(new Date().getTime() / 1000)
+    }
 
     // Check for location data
-    let latitude = formData.get('latitude') || null
-    let longitude = formData.get('longitude') || null
-
-    if (latitude == 'Location not retrieved') latitude = null
-    if (longitude == 'Location not retrieved') longitude = null
+    if (submissionData.latitude == 'Location not retrieved') submissionData.latitude = null
+    if (submissionData.longitude == 'Location not retrieved') submissionData.longitude = null
 
     // Do some content type checks
-    let email = formData.get('email', null)
-    if (email !== null && !isEmail(email)) {
+    if (submissionData.email !== null && !isEmail(submissionData.email)) {
         resp.message = `You provided an e-mail address that is invalid`
         return new Response(JSON.stringify(resp), {status: 400, headers: {'Content-Type': 'application/json'}})
     }
 
     // And get ready to upload our files
-    let files = [];
     let photos = formData.getAll('photos', []);
     if (photos.length > 0) {
         // We allow at maximum 5 photos
@@ -63,7 +72,7 @@ export async function handle({ request, env }) {
             let uuid = nanoid();
             let data = await p.arrayBuffer();
             await env.R2.put(uuid, data);
-            files.push(uuid);
+            submissionData.files.push(uuid);
         }
     }
 
@@ -71,19 +80,19 @@ export async function handle({ request, env }) {
     let query;
     try {
         query = await env.DB.prepare('INSERT INTO SOSRequest (first_name, last_name, others_name, email, phone_number, location_description, need, other_need, us_citizen, latitude, longitude, photo_urls, created_at) VALUES (?1, ?2, ?3, ?4, ?5, ?6, ?7, ?8, ?9, ?10, ?11, ?12, ?13)').bind(
-            formData.get('firstName', ''),
-            formData.get('lastName', ''),
-            formData.getAll('othersName').join(',') || '',
-            email,
-            formData.get('phoneNumber', ''),
-            formData.get('locationDescription', ''),
-            formData.get('need', ''),
-            formData.get('otherNeed', ''),
-            formData.get('usCitizen', 'No'),
-            latitude,
-            longitude,
-            files.join(','),
-            submission_time
+            submissionData.firstName,
+            submissionData.lastName,
+            submissionData.othersName,
+            submissionData.email,
+            submissionData.phoneNumber,
+            submissionData.locationDescription,
+            submissionData.need,
+            submissionData.otherNeed,
+            submissionData.usCitizen,
+            submissionData.latitude,
+            submissionData.longitude,
+            submissionData.files.join(','),
+            submissionData.createdAt
         ).run()
     }
     catch(e) {
@@ -101,9 +110,9 @@ export async function handle({ request, env }) {
     if (query.success && env.SLACK_WEBHOOK) {
         let google_maps_link = "None"
         let location_display = "None"
-        if (latitude && longitude) {
-            google_maps_link = `https://www.google.com/maps/search/${latitude},${longitude}`
-            location_display = `<${google_maps_link}|Map> - Latitude: ${latitude}, Longitude: ${longitude}`
+        if (submissionData.latitude && submissionData.longitude) {
+            google_maps_link = `https://www.google.com/maps/search/${submissionData.latitude},${submissionData.longitude}`
+            location_display = `<${google_maps_link}|Map> - Latitude: ${submissionData.latitude}, Longitude: ${submissionData.longitude}`
         }
 
         let file_display = []
@@ -111,18 +120,18 @@ export async function handle({ request, env }) {
             file_display.push(`https://refugeeaid-pages.pages.dev/uploads/${f}`)
         }
 
-        let slackBody = `*Date (UTC)*: ${new Date(submission_time * 1e3).toISOString()}
-*First Name*: ${formData.get('firstName', '')}
-*Last Name*: ${formData.get('lastName', '')}
-*Other Names*: ${formData.getAll('othersName').join(',') || ''}
-*Email*: ${email}
-*Phone Number*: ${formData.get('phoneNumber', '')}
-*Location Description*: ${formData.get('locationDescription', '')}
-*Need*: ${formData.get('need', '')}
-*Other Need*: ${formData.get('otherNeed', '')}
-*US Citizen*: ${formData.get('usCitizen', 'No')}
+        let slackBody = `*Date (UTC)*: ${new Date(submissionData.createdAt * 1e3).toISOString()}
+*First Name*: ${submissionData.firstName}
+*Last Name*: ${submissionData.lastName}
+*Other Names*: ${submissionData.othersName}
+*Email*: ${submissionData.email}
+*Phone Number*: ${submissionData.phoneNumber}
+*Location Description*: ${submissionData.locationDescription}
+*Need*: ${submissionData.need}
+*Other Need*: ${submissionData.otherNeed}
+*US Citizen*: ${submissionData.usCitizen}
 *Location*: ${location_display}
-*Photo URLs*: ${file_display.join(', ')}
+*Photo URLs*: ${file_display.join(',')}
 `
 
         let slack = await fetch(env.SLACK_WEBHOOK, {
